5b135bb60713715e1e0435a75a31c08a70d8fe0d
Jason Drain <jason.bensley.drain@gmail.com>
2021-07-05 17:24:04 -0500

    Initialize repo and add some of the server code

---
+++ server/lib/countryDataProvider.js
@@ -0,0 +1,11 @@
+var countryDataProvider = {}
+
+countryDataProvider.getCountryData = (countryData) => {
+    // use restCountryGateway to get all the data
+
+    // sort and filter the data here
+
+    // 
+}
+
+module.exports = countryDataProvider
